39dea904a9f1b05e21532efd14e1833828021d19
Coosanta17 <coosantamapy@gmail.com>
2024-05-16 18:48:45 +1000

    Generated template.

---
+++ src/main/java/net/coosanta/random_shit/CoosantasRandomShitDataGenerator.java
@@ -0,0 +1,11 @@
+package net.coosanta.random_shit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoosantasRandomShitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
