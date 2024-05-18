d9fd01af4a99df6dd28472e7ff9dcca345a89f60
Coosanta17 <coosantamapy@gmail.com>
2024-05-18 14:47:56 +1000

    Added Comments to Give Credit.

--- src/main/java/net/coosanta/random_shit/block/ModBlocks.java
+++ src/main/java/net/coosanta/random_shit/block/ModBlocks.java
@@ -17,7 +17,7 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).mapColor(MapColor.PALE_YELLOW)));
     public static final Block SHITSTAINED_WOOL = registerBlock("shitstained_wool",
             new Block(FabricBlockSettings.copyOf(Blocks.BROWN_WOOL)));
-    // ^^ Textures of these blocks taken from https://www.planetminecraft.com/project/the-backrooms-5763207/
+    // ^^ Textures of these blocks taken (stolen) from https://www.planetminecraft.com/project/the-backrooms-5763207/
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
